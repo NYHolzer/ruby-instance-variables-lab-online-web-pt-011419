3744e4170c6a63976c8ff70c663486e6f55fad70
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-01 02:43:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def name=(dog_name)       #takes in argument and sets it to variable
-    this_dogs_name = dog_name 
+    @this_dogs_name = dog_name 
   end
   
   def name      #reporting / reading the name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
